9f344f96558e80f1973d4a7945c7b6964974beca
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-17 22:23:26 +0530

    Working Code Proper

--- Calculator.java
+++ Calculator.java
@@ -17,76 +17,84 @@ class Functions{
         return a/b;
     }}
 
+   class input extends Functions{
+        int num1, num2;
+        Scanner sc = new Scanner(System.in);
+        
+        void getInput(){
+            System.out.print("Enter number 1: ");
+            num1 = sc.nextInt();
+            
+            System.out.print("Enter number 2: ");
+            num2 = sc.nextInt();
+        }
+    }
 
-class Calculator{
+
+public class Calculator{
     public static void main(String[] args) {
 
         int option; 
+        option = 0;
         Scanner sc = new Scanner(System.in);
-
-        // Display Options
-        System.out.println("Choose Options: 1,2,3,4");
+       
+         
+        // Object of Functions class
+        Functions fn = new Functions();
+        input inp = new input();
+        
+        
+        // Cases 
+        while(option != 5){
+             // Display Options
+        System.out.println("\nChoose Options: 1,2,3,4");
         System.out.println("1: Addition");
         System.out.println("2: Subtraction");
         System.out.println("3: Multiplication");
         System.out.println("4: Division");
         System.out.println("5: Exit");
+
+        // Input Option
         System.out.print("Enter option: ");
         option = sc.nextInt();
-         
-        // Object of Functions class
-        Functions fn = new Functions();
-        
-        
-        // Cases
+
+
+        double result;
+
+
         switch(option){
             case 1:
-                System.out.print("Enter number 1: ");               
-                int num1 = sc.nextInt();
-                
-                System.out.print("Enter number 2: ");
-                int num2 = sc.nextInt();
-                
-                double result = fn.sum(num1, num2);
+                inp.getInput();
+                result = fn.sum(inp.num1, inp.num2);
                 System.out.println("Result is: "+ result);
                 break;
 
             case 2: 
-                System.out.print("Enter number 1: ");
-                num1 = sc.nextInt();
-                
-                System.out.print("Enter number 2: ");
-                num2 = sc.nextInt();
-                
-                result = fn.sub(num1, num2);
+                inp.getInput();
+                result = fn.sub(inp.num1, inp.num2);
                 System.out.println("Result is: "+ result);
                 break;
 
             case 3: 
-                System.out.print("Enter number 1: ");
-                num1 = sc.nextInt();
-                
-                System.out.print("Enter number 2: ");
-                num2 = sc.nextInt();
-                
-                result = fn.multi(num1, num2);
+                inp.getInput();
+                result = fn.multi(inp.num1, inp.num2);
                 System.out.println("Result is: "+ result);
                 break;
 
             case 4:
-                System.out.print("Enter number 1: ");                
-                num1 = sc.nextInt();
-                
-                System.out.print("Enter number 2: ");
-                num2 = sc.nextInt();
-                
-                result = fn.divide(num1, num2);
+                inp.getInput();
+                result = fn.divide(inp.num1, inp.num2);
                 System.out.println("Result is: "+ result);
+                break;
+
+            case 5:
+                System.out.println("Exiting the program.");
                 break;
 
             default:
             System.out.println("Error: Invalid Option");
-        }
-        
+            
+        }}
+        sc.close();
     }
 }
