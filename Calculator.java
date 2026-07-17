9b296de78782fc8967d41beffd7688d4ecd20330
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-17 21:23:41 +0530

    Menu Options

--- Calculator.java
+++ Calculator.java
@@ -19,6 +19,15 @@ class Functions{
 
 class Calculator{
     public static void main(String[] args) {
+        // Display Options
+        System.out.println("Choose Options: 1,2,3,4");
+        System.out.println("1: Addition");
+        System.out.println("2: Subtraction");
+        System.out.println("3: Multiplication");
+        System.out.println("4: Division");
+        System.out.println("5: Exit");
+        
+       
         
     }
 }
