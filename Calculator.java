f0ec496334e4d66db68c2e99d149fd9da0925bf0
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-17 21:13:13 +0530

    Initial Setup

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+class Functions{
+    void sum(int a, int b){
+        System.out.println("Sum is: " + (a+b));
+    }}
+
+
+
+class Calculator{
+    public static void main(String[] args) {
+        
+    }
+}
